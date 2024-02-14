5ac363f77d64a2b376b4fcfab12b1cb75ca6f501
Kulimák Máté <kulimakmate@turr.hu>
2024-02-14 11:33:08 +0100

    Input mező design elkezdése

---
+++ src/components/Input.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <input type="text" name="" id="mezo">
+</template>
+
+<style>
+
+   input{
+    width: 50%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+   }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Input from '@/components/Input.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Input />
   </main>
 </template>
