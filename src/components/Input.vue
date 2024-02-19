5bd14b5a4f443437ff80ca674081d058ce4cc972
Kulimák Máté <kulimakmate@turr.hu>
2024-02-19 08:44:40 +0100

    Majdnem kész van az input mező jeje

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-    <input type="text" name="" id="mezo">
+    <input type="text" name="" class="mezo" id="alap" placeholder="Placeholder">
+    <br>
+    <input type="text" name="" class="mezo" id="disabled" placeholder="Placeholder" disabled>
+    <br>
+    <input type="text" name="" class="mezo" id="aktiv" placeholder="Placeholder">
 </template>
 
 <style>
@@ -13,8 +17,24 @@
     margin: 8px 0;
     display: inline-block;
     border: 1px solid #ccc;
-    border-radius: 4px;
+    border-radius: 7px;
     box-sizing: border-box;
+    box-shadow: 4px 4px 8px  rgba(0, 0, 0, 0.24);
+    
+   }
+   input[type="text"]:disabled {
+    background: #E5E5E5;
+    
+   }
+   ::placeholder {
+    color: #D4D4D4;
+   }   
+   input[type="text"]:disabled::placeholder{
+       color: #A3A3A3;
    }
 
+   input:focus{
+       outline: none !important;
+       border-color: #FB923C;
+   }
 </style>
