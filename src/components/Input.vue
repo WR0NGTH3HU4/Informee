67cfeb6e84295f66dfebad559f3c1e2f167cd1b2
Kulimák Máté <kulimakmate@turr.hu>
2024-02-20 08:42:47 +0100

    Megírtam azt a js kódot ami azt vizsgálja hogy az input mező aktív e

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,40 +1,48 @@
-<script setup lang="ts">
-</script>
-
 <template>
-    <input type="text" name="" class="mezo" id="alap" placeholder="Placeholder">
-    <br>
-    <input type="text" name="" class="mezo" id="disabled" placeholder="Placeholder" disabled>
-    <br>
-    <input type="text" name="" class="mezo" id="aktiv" placeholder="Placeholder">
+    <div>
+      <input type="text" name="" class="mezo" id="alap" placeholder="Placeholder" @focus="toggleActive(true)" @blur="toggleActive(false)">
+      <br>
+      <input type="text" name="" class="mezo" id="disabled" placeholder="Placeholder" disabled>
+      <br>
+      <input type="text" name="" class="mezo" id="aktiv" placeholder="Placeholder" @focus="toggleActive(true)" @blur="toggleActive(false)">
+      
+    </div>
 </template>
+  
+  <script setup lang="ts">
+    let isActive = false;
+  
+    const toggleActive = (value: boolean) => {
+      isActive = value;
+      console.log('Input mező:', isActive);
+    };
 
-<style>
-
-   input{
-    width: 50%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 7px;
-    box-sizing: border-box;
-    box-shadow: 4px 4px 8px  rgba(0, 0, 0, 0.24);
-    
-   }
-   input[type="text"]:disabled {
-    background: #E5E5E5;
-    
-   }
-   ::placeholder {
-    color: #D4D4D4;
-   }   
-   input[type="text"]:disabled::placeholder{
-       color: #A3A3A3;
-   }
-
-   input:focus{
-       outline: none !important;
-       border-color: #FB923C;
-   }
-</style>
+  </script>
+  
+  <style scoped>
+    .mezo {
+      width: 50%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 7px;
+      box-sizing: border-box;
+      box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+    }
+    input[type="text"]:disabled {
+      background: #E5E5E5;
+    }
+    ::placeholder {
+      color: #D4D4D4;
+    }
+    input[type="text"]:disabled::placeholder {
+      color: #A3A3A3;
+    }
+    input:focus {
+      outline: none !important;
+      border-color: #FB923C;
+      box-shadow: 0 4px 6px -1px rgba(94, 78, 58, 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+    }
+  </style>
+  
