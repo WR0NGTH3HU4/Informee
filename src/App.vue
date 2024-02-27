e1efb870886cc3beccaf72a997f72e6a6d68eb33
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2024-02-27 08:41:25 +0100

    Post action button file létrehozása, gombok elkezdése

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 //import HelloWorld from './components/HelloWorld.vue'*/
-import Logo from './components/Logo.vue';
+import Logo from './components/Logo.vue'
 import Navbar from './components/Navbar.vue'
-import Button from './components/Button.vue';
-function asd() {
-    alert("asdasdasd")
-}  
+import Button from './components/Button.vue'
 
+function asd() {
+  alert('asdasdasd')
+}
 </script>
 
 <template>
-    <Navbar/>
-    <Button text="Primary button" type="primary"></Button>
-    <Button text="Danger button" type="danger"></Button>
-    <RouterView/>
+  <Navbar />
+  <Button text="Primary button" type="primary"></Button>
+  <Button text="Danger button" type="danger"></Button>
+  <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/PostActionButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <button :class="type" @click="click">{{ text }}</button>
+</template>
+
+<style scoped>
+    .upVote {
+        border-radius: 10px;
+    }
+   
+</style>
