5b12d75d25d9dcd8bb7ca1c678e7f02130f54713
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2024-03-11 08:46:07 +0100

    gombok meg ilyenek

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 //import HelloWorld from './components/HelloWorld.vue'*/
 import Logo from './components/Logo.vue'
 import Navbar from './components/Navbar.vue'
-import Button from './components/Button.vue'
-import PostActionButton from './components/PostActionButton.vue'
 
 function asd() {
   alert('asdasdasd')
@@ -13,19 +11,10 @@ function asd() {
 
 <template>
   <Navbar />
-
-  <Button text="Primary button" type="primary"></Button>
-  <Button text="Danger button" type="danger"></Button>
-  <PostActionButton class="upvote"></PostActionButton>
-  <PostActionButton class="dislike"></PostActionButton>
-  <PostActionButton class="share"></PostActionButton>
-  <PostActionButton class="report"></PostActionButton>
-  <RouterView />
-=======
   <!--<Button text="Primary button" type="primary"></Button>-->
   <!--<Button text="Danger button" type="danger"></Button>-->
   <!--<PostActionButton class="upvote"></PostActionButton>-->
-  <RouterView class="flex"/>
+  <RouterView class=""/>
 
 </template>
 
--- src/views/Kezdolap.vue
+++ src/views/Kezdolap.vue
@@ -1,5 +1,17 @@
+<script setup>
+import Button from '../components/Button.vue'
+import PostActionButton from '../components/PostActionButton.vue'
+</script>
+
 <template>
     <div>
-        <h2>Kezdolap</h2>
+        <h2>kezdes lapja</h2>
+        
+  <Button text="Primary button" type="primary"></Button>
+  <Button text="Danger button" type="danger"></Button>
+  <PostActionButton class="upvote"></PostActionButton>
+  <PostActionButton class="dislike"></PostActionButton>
+  <PostActionButton class="share"></PostActionButton>
+  <PostActionButton class="report"></PostActionButton>
     </div>
 </template>
