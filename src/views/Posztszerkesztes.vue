2a1aafb5db8d42c4ead0b448e64cca3245c21e07
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2024-03-12 15:49:43 +0100

    adding view to router(with public meta)

---
+++ src/views/Posztszerkesztes.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+    import {ref} from 'vue'
+    import Button from '../components/Button.vue'
+    
+    import Input from '@/components/Input.vue';
+</script>
+<template>
+<!--Posztszerkesztő-->
+<!--Feltöltött anyagok-->
+<!--Anyag feltöltése-->
+</template>
+<style>
+
+</style>
