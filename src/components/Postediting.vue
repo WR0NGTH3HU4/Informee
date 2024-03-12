a3adc61d3a9fb52d19d844dbc550af32d06dc8e7
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2024-03-12 16:36:30 +0100

    inserted postediting with fix sized "tartalom" input

---
+++ src/components/Postediting.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+    import {ref} from 'vue'
+    import Input from './Input.vue';
+</script>
+<template>
+    
+    <div class="container">
+            <div class="cim Title">
+                <h3>Cím</h3>
+                <Input id="cim" placeholder="Cím"/>
+            </div>
+            <div class="leiras Title">
+                <h3>Leírás</h3>
+                <Input id="leriras" placeholder="Leírás"/>
+            </div>
+            <div class="tartalom Title">
+                <h3>Tartalom</h3>
+                <Input id="tartalom" placeholder="Tartalom"/>
+            </div>
+    </div>
+</template>
+<style scoped>
+    .container{
+        @apply flex flex-col justify-center content-center
+    }
+    #tartalom{
+        height: 520px;
+    }
+    .Title{
+        @apply text-neutral-500
+    }
+</style>
--- src/views/Posztszerkesztes.vue
+++ src/views/Posztszerkesztes.vue
@@ -1,15 +1,38 @@
-<script lang="ts">
+<script lang="ts" setup>
     import { RouterLink, RouterView } from 'vue-router'
     import {ref} from 'vue'
     import Button from '../components/Button.vue'
     
-    import Input from '@/components/Input.vue';
+    import Postediting from '@/components/Postediting.vue';
 </script>
 <template>
-<!--Posztszerkesztő-->
-<!--Feltöltött anyagok-->
-<!--Anyag feltöltése-->
+
+    <div class="head">
+        <h1 class="text-neutral-800 text-4xl">Posztszerkesztő</h1>
+        <div class="validating">
+            <Button type="secondary" text="Módosítások eldobása"></Button>
+            <Button id="Mentes" type="primary" text="Mentés"></Button>
+        </div>
+    </div>
+    <!--Posztszerkesztő-->
+    <Postediting/>
+    <!--Feltöltött anyagok-->
+    <!--Anyag feltöltése-->
+
 </template>
 <style>
 
+    .head{
+        @apply w-full flex flex-row justify-between content-center items-center;
+        padding: 0px 5px 0px 10px;
+        margin-top: 3%;
+        margin-bottom: 3%;
+    }
+    .validating{
+       @apply flex flex-row justify-between content-center items-center;
+       
+    }
+    #Mentes{
+        margin: 0 10px;
+    }
 </style>
