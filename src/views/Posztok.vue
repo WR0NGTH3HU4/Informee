59bf16f0957517045f46fd788487c5a3bf99f11a
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2024-03-17 10:35:50 +0100

    searchbar design complete

--- src/views/Posztok.vue
+++ src/views/Posztok.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
     import Sidebar from '@/components/Sidebar.vue';
+    import Searchbar from '@/components/Searchbar.vue';
 </script>
 <template>
     <div class="FullPostPage">
@@ -7,7 +8,16 @@
         <Sidebar/>
         <div class="Posts">
             <!--Searchbar-->
+            <Searchbar/>
             <!--Postlist-->
         </div>
     </div>
 </template>
+<style lang="scss" scoped>
+    .FullPostPage{
+        @apply flex flex-row justify-start content-center items-start
+    }
+    .Posts{
+        @apply w-full
+    }
+</style>
