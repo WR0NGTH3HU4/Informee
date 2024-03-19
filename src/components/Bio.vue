8979eaf5a7227606420e4a882e2220f1c9c7328c
KovagoLevente <kovagolevente@turr.hu>
2024-03-19 08:46:06 +0100

    bio component létrehozása

---
+++ src/components/Bio.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import Button from '../components/Button.vue'
+
+</script>
+<template>
+<div class="full_bio">
+    
+    <div class="fejlec">
+        <img src="../assets/fooldal_logo.svg" alt="">
+        <div class="name">
+            <h4>molnarkiki</h4>
+            <h3>Molnár Krisztián</h3>
+        </div>
+        
+    </div>
+    <div class="bio">
+        <div>
+            <h3>Bio</h3>
+        </div>
+
+    </div>
+</div>
+
+
+</template>
+<style lang="scss">
+    .full_bio{
+        @apply flex flex-col justify-center content-center ;
+        background-color:lightblue;
+    }
+    .fejlec{
+        @apply flex flex-row justify-center content-center ;
+        background-color: cadetblue;
+        //padding: 30px;
+        width: 100%;
+        height: 40%;
+    }
+    img{
+        width: 8rem;
+        padding: 10px;
+    }
+    .name{
+        @apply flex flex-col justify-center content-center ;
+    }
+</style>
