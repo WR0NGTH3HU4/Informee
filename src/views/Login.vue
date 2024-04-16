bbf7c1181cfa9dc97d8bfccb6cc858102e37e178
csandrisTurr <csaszarandras@turr.hu>
2024-04-16 08:56:47 +0200

    szebb login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -39,10 +39,10 @@
     })
 </script>
 <template>
-    <div class="FullPage">
-        <div class="loginWindow">
-            <div class="TitleContainer">
-                <h1 class="Title" id="Title">Bejelentkezés</h1>
+    <main class="flex justify-center items-center h-full">
+        <div class="flex flex-col p-6 bg-neutral-100 shadow-md justify-between items-center content-center border border-neutral-300 rounded-lg gap-4">
+            <div class="w-full flex justify-start">
+                <h1 class="text-neutral-800 text-3xl" id="Title">Bejelentkezés</h1>
             </div>
             <div class="InputContainer">
                 <h3 class="inputName">E-mail</h3>
@@ -60,36 +60,16 @@
                     </div>
                 </div>
             </div>
-            <div class="GoToPages">
+            <div class="flex flex-row justify-between content-center items-center w-full gap-4">
                 <h5 class="text-neutral-500 font-medium flex flex-row">Nincs még fiókod?<RouterLink to="/Registration"><h5 class="text-orange-500 underline" id="Reg">Regisztrálj!</h5></RouterLink></h5> 
                 <Button text="Bejelentkezés" type="primary" @click.prevent="onLogIn"></Button>
             </div>
         </div>
-    </div>
+    </main>
 </template>
 <style scoped lang="scss">
-
-    .FullPage{
-        @apply flex flex-col justify-center content-center items-center h-screen
-    }
-    .loginWindow{
-        @apply rounded-lg  border-neutral-300 bg-neutral-100 drop-shadow-lg flex flex-col justify-between items-center content-center;
-        width: 30%;
-        height: 40%;
-        border-width: 1px;
-        padding: 1.5rem;
-    }
-    .TitleContainer{
-        @apply w-full flex justify-start; 
-    }
     .InputContainer{
         @apply w-full flex flex-col justify-center content-center
-    }
-    .GoToPages{
-        @apply flex flex-row justify-between content-center items-center w-full
-    }
-    .Title{
-        @apply text-neutral-800 text-3xl;
     }
     .inputName{
         @apply text-neutral-500
