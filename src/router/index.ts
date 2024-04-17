b33a13bf0b5e8e50294aee714627d8821421bf3f
TatraiDominik <tatraidominikoszkar@turr.hu>
2024-04-17 11:34:59 +0200

    added the basic divs to html

--- src/router/index.ts
+++ src/router/index.ts
@@ -91,6 +91,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path:'/Login',
+      name:'Login',
+      component: () => import('../views/Profile.vue'),
+      meta:{
+        public: true
+      }
     }
   ]
 });
---
+++ src/views/Profile.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+    import Bio from "../components/Bio.vue"
+</script>
+<template>
+    <div class="FullPage">
+        <div class="Banner">
+            <div class="ProfilePic"></div>
+        </div>
+        <div class="Infos">
+            <h3>Profilkép:</h3>
+            <div class="PhotoButtons">
+
+            </div>
+            <h3>Bio</h3>
+            <div>
+
+            </div>
+            <h3>Linkek</h3>
+            <div>
+
+            </div>
+            <div>
+                <h3>Link:</h3>
+                <h3>Hozzáad</h3>
+            </div>
+            <div>
+
+            </div>
+            
+            
+        </div>
+        <div class="Link">
+            <div class="ProfileButtons"></div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
