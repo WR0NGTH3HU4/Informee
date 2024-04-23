58b8d6667d3ae286d1f1d7a482d1175f6b9b831d
csandrisTurr <csaszarandras@turr.hu>
2024-04-23 08:31:21 +0200

    szebb fooldal design

--- src/views/Kezdolap.vue
+++ src/views/Kezdolap.vue
@@ -12,17 +12,21 @@ import Button from '@/components/Button.vue';
       </div>
     </div>
 
-    <div class="tartalom">
+    <div class="flex flex-row justify-evenly content-center items-center h-full">
 
-      <div class="tartalom-bal">
-        <h1>Valami menő text</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quam consectetur dolore quidem quae? Laboriosam assumenda, ducimus illum molestias quo in illo quis pariatur accusamus consequuntur obcaecati, provident fugit? Aut.</p>
-        <Button class="primary" text="Bejelentkezés"></Button>
-        <h5 class="text-neutral-500 font-medium flex flex-row">vagy <RouterLink to="/Registration"><h5 class="text-neutral-500 underline" id="Reg"> Regisztrálj!</h5></RouterLink></h5>
+      <div class="flex flex-col w-[600px] gap-8">
+        <h1 class="text-neutral-800 text-6xl">Valami menő text</h1>
+        <p class="text-neutral-700 font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quam consectetur dolore quidem quae? Laboriosam assumenda, ducimus illum molestias quo in illo quis pariatur accusamus consequuntur obcaecati, provident fugit? Aut.</p>
+        <span class="text-neutral-500 font-medium flex flex-row items-center gap-4">
+          <Button class="primary" text="Jelentkezz be"></Button>
+          <RouterLink to="Regisztracio">
+            <span class="text-neutral-500 underline" id="Reg">vagy regisztrálj!</span>
+          </RouterLink>
+        </span>
       </div>
       
 
-      <div class="tartalom-jobb">
+      <div class="flex flex-col justify-center content-center items-center aspect-square w-[500px]">
         <img src="../assets/fooldal_logo.svg" alt="">
       </div>
     </div>
@@ -38,38 +42,16 @@ import Button from '@/components/Button.vue';
 <style scoped>
     .hullamocskak {
       position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-image: url('../assets/hullamcock.svg');
-    background-size: cover; /* Adjusts background size to cover entire container */
-    background-position: center; /* Centers the background image */
-    z-index: -1;
-    }
-     .tartalom{
-      @apply flex flex-row justify-between content-center items-center;
-      width: 100%;
-      padding: 10%;
-    }
-    
-    .tartalom-jobb{
-      @apply flex flex-col justify-center content-center items-center;
-    }
-    .tartalom-bal{
-      @apply flex flex-col justify-center content-center items-center w-full
-    }
-    h1{
-      @apply text-7xl text-neutral-800;
-      margin-bottom: 80px
-    }
-    #Reg{
-        padding-left: 5px;
-    }
-    svg{
+      top: 0;
+      left: 0;
       width: 100%;
       height: 100%;
+      background-image: url('../assets/hullamcock.svg');
+      background-size: cover; /* Adjusts background size to cover entire container */
+      background-position: center; /* Centers the background image */
+      z-index: -1;
     }
+
     .hatter{
       width: 100%;
     }
