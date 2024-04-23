e6b52ef45325386f786c1c1b60ef62871fcdf404
TatraiDominik <tatraidominikoszkar@turr.hu>
2024-04-23 08:40:05 +0200

    profile edit bg added

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,38 +2,48 @@
     import Bio from "../components/Bio.vue"
 </script>
 <template>
-    <div class="FullPage">
-        <div class="Banner">
-            <div class="ProfilePic"></div>
-        </div>
-        <div class="Infos">
-            <h3>Profilkép:</h3>
-            <div class="PhotoButtons">
+    <div class="FullPage w-full h-full ">
+        <div class="Profile flex flex-col justify-center content-center items-start bg-neutral-50 ">
+            <div class="Banner">
+                <div class="ProfilePic"></div>
+            </div>
+            <div class="Infos flex flex-col justify-center content-center items-center w-full">
+                <h3>Profilkép:</h3>
+                <div class="PhotoButtons"></div>
+                <h3>Bio</h3>
+                <div>
 
-            </div>
-            <h3>Bio</h3>
-            <div>
+                </div>
+                <h3>Linkek</h3>
+                <div>
 
-            </div>
-            <h3>Linkek</h3>
-            <div>
+                </div>
+                <div>
+                    <h3>Link:</h3>
+                    <h3>Hozzáad</h3>
+                </div>
+                <div>
 
+                </div>
+                
+                
             </div>
-            <div>
-                <h3>Link:</h3>
-                <h3>Hozzáad</h3>
+            <div class="Link">
+                <div class="ProfileButtons"></div>
             </div>
-            <div>
-
-            </div>
-            
-            
-        </div>
-        <div class="Link">
-            <div class="ProfileButtons"></div>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-
+    .Profile{
+        
+        border: 1px solid;  
+        @apply border-neutral-300 rounded-lg drop-shadow-md
+    }
+    .FullPage{
+        padding: 10%;
+    }
+    .ProfilePic{
+        background-image: url("../assets/logo.svg")
+    }
 </style>
