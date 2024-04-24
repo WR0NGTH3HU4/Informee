ddf344fec7f991d7abec89b71854f7b7ad28aece
csandrisTurr <csaszarandras@turr.hu>
2024-04-24 11:35:11 +0200

    api wrapper staticca tevese

--- src/composables/ApiWrapper.ts
+++ src/composables/ApiWrapper.ts
@@ -7,33 +7,33 @@ export interface ApiResponse<T> {
 }
 
 export class ApiWrapper {
-    private readonly BACKEND_URL = "http://127.0.0.1:3000/";
+    private static readonly BACKEND_URL = "http://127.0.0.1:3000/";
 
-    public async get<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
+    public static async get<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
         return await this.request<T>('GET', route, data);
     }
 
-    public async post<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
+    public static async post<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
         return await this.request<T>('POST', route, data);
     }
 
-    public async patch<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
+    public static async patch<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
         return await this.request<T>('PATCH', route, data);
     }
 
-    public async put<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
+    public static async put<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
         return await this.request<T>('PUT', route, data);
     }
 
-    public async delete<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
+    public static async delete<T = any>(route: string, data: any): Promise<ApiResponse<T>> {
         return await this.request<T>('DELETE', route, data);
     }
 
-    private async request<T = any>(method: string, route: string, data: any): Promise<ApiResponse<T>> {
+    private static async request<T = any>(method: string, route: string, data: any): Promise<ApiResponse<T>> {
         const res = await axios.request({
             method,
             url: route,
-            baseURL: this.BACKEND_URL,
+            baseURL: ApiWrapper.BACKEND_URL,
             data,
         })
 
