3f65ead477731732c0bf4fc702e240830707fde5
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2024-05-19 09:30:10 +0200

    boxes can be opened separetely with questions unfolding

--- src/views/GyakranIsmetelt.vue
+++ src/views/GyakranIsmetelt.vue
@@ -2,10 +2,9 @@
 import FAQBox from '../components/FAQBox.vue'
 </script>
 <template>
-    <div class="flex flex-row flex-wrap justify-center content-center items-center w-full h-full gap-10">
+    <div class="flex flex-row justify-center content-center items-center w-full h-full gap-10">
         <FAQBox></FAQBox>
-        <FAQBox></FAQBox>
-        <FAQBox></FAQBox>
+
     </div>
     
 </template>
